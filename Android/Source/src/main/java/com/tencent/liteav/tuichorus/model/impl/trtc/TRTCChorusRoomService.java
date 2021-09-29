61aba2542df351e45b39f6d69cdf5f3061823f58
tatemin <tatemin@tencent.com>
2021-09-29 15:25:39 +0800

    Android移除androidx迁移说明&增加TUI组件优化参数

--- Android/Source/src/main/java/com/tencent/liteav/tuichorus/model/impl/trtc/TRTCChorusRoomService.java
+++ Android/Source/src/main/java/com/tencent/liteav/tuichorus/model/impl/trtc/TRTCChorusRoomService.java
@@ -84,11 +84,25 @@ public class TRTCChorusRoomService extends TRTCCloudListener {
         internalEnterRoom();
     }
 
+    private void setFramework(int framework) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("api", "setFramework");
+            JSONObject params = new JSONObject();
+            params.put("framework", framework);
+            jsonObject.put("params", params);
+            mTRTCCloud.callExperimentalAPI(jsonObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void internalEnterRoom() {
         // 进房前设置一下监听，不然可能会被其他信息打断
         if (mTRTCParams == null) {
             return;
         }
+        setFramework(5);
         mTRTCCloud.setListener(this);
         mTRTCCloud.enterRoom(mTRTCParams, TRTCCloudDef.TRTC_APP_SCENE_VOICE_CHATROOM);
         // enable volume callback
